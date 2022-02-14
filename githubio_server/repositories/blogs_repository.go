ae9a015f95302f0dd829f03a7cacb5d494942347
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-02-14 22:52:26 +0900

    return s3 object urls by repostiroies

--- githubio_server/repositories/blogs_repository.go
+++ githubio_server/repositories/blogs_repository.go
@@ -8,7 +8,9 @@ func FetchBlogs() []string {
 	var blog_urls []string
 	blog_files := infra.FetchS3Objects("md-host-bucket")
 	for _, blog_file := range blog_files {
-		blog_urls = append(blog_urls, infra.FetchUrlOfS3Object("md-host-bucket", blog_file))
+		blog_urls = append(
+			blog_urls, infra.FetchUrlOfS3Object("md-host-bucket", blog_file),
+		)
 	}
 	return blog_urls
 }
