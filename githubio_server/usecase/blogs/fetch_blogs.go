7cd04294d354119ae12a1ac3e31f3fca386beb5d
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-02-17 11:58:40 +0900

    chage blog_title value

--- githubio_server/usecase/blogs/fetch_blogs.go
+++ githubio_server/usecase/blogs/fetch_blogs.go
@@ -1,6 +1,8 @@
 package blogs
 
 import (
+	"strings"
+
 	"github.com/yuta519/githubio_server/domain"
 	"github.com/yuta519/githubio_server/repositories"
 )
@@ -9,10 +11,11 @@ func FetchBlogs() []domain.Blog {
 	var blogs []domain.Blog
 	blog_files := repositories.FetchBlogFiles()
 	for _, blog_file := range blog_files {
+		blog_title := strings.Replace(blog_file, "_", " ", -1)
 		blogs = append(
 			blogs,
 			domain.Blog{
-				Title: repositories.GetFileNameWithoutExtension(blog_file),
+				Title: repositories.GetFileNameWithoutExtension(blog_title),
 				Url:   repositories.GgetUrlOfBlogFile(blog_file),
 			},
 		)
